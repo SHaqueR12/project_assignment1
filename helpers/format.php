1f9f98d51e364aa47ffb5da716560c98df137376
SHaqueR12 <109901984+SHaqueR12@users.noreply.github.com>
2022-10-30 10:06:39 +0600

    Add files via upload

---
+++ helpers/format.php
@@ -0,0 +1,45 @@
+<?php
+
+class format 
+{
+public function formatdate($date)
+{
+
+return date('F J, Y, g:i a',strtotime($date));
+}
+public function textshorten($text,$limit=500)
+{
+$text=$text." ";
+$text=substr($text,0,$limit);
+$text=substr($text,0,strrpos($text, ' ' ));
+$text=$text."......";
+
+return $text;
+}
+public static function validation($data)
+{
+$data=trim($data);
+$data=stripslashes($data);
+$data=htmlspecialchars($data);
+return $data;
+}
+
+public function title()
+{
+    $path = $_SERVER['SCRIPT_FILENAME'];
+    $title = basename($path, '.php');
+    //$title=str_replace('_', ' ', $title);
+    if($title == 'index')
+    {
+        $title = 'home';
+    }
+    elseif($title == 'contact')
+    {
+        $title = 'contact';
+    }
+    return $title = ucwords($title) ; 
+}
+
+}
+
+?>
